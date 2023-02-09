7c9837bea1b121e51e96aeceb1b91a52f7c38431
William <william310391@gmail.com>
2023-02-09 13:42:42 -0500

    sssss

--- src/main/java/com/example/api/repository/UsuarioRepository.java
+++ src/main/java/com/example/api/repository/UsuarioRepository.java
@@ -7,9 +7,6 @@ import com.example.api.entity.Usuario;
 @Service
 public interface UsuarioRepository extends BaseRepository<Usuario> {
   public Usuario findOneEmail(String email);
-  // public void save2 (Usuario usuario);
-
-
-  
+  // public void save2 (Usuario usuario);  
 }
 
